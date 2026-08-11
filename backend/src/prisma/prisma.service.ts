f4f676dfda95803a565c8d806b2ef6d66fb6a43b
Muhammad_Tamim633 <tamimsatti455@gmail.com>
2026-08-11 12:00:07 +0500

    Use native prisma client with DATABASE_URL

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,22 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const adapter = new PrismaMariaDb({
-      host: process.env.MYSQLHOST!,
-      port: Number(process.env.MYSQLPORT!) || 3306,
-      user: process.env.MYSQLUSER!,
-      password: process.env.MYSQLPASSWORD!,
-      database: process.env.MYSQLDATABASE!,
-      connectionLimit: 5,
-    });
-    
-    super({ adapter });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
